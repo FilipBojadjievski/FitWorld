ad9e7b66f88f1652d02e48fe57afc8c5fe57513d
Ema Arsova <arsovaema@gmail.com>
2026-06-22 11:55:33 +0200

    Add live input sorting filter to available gyms catalog

--- Model/signup.php
+++ Model/signup.php
@@ -1,5 +1,4 @@
 <?php
-// Controller/SignupController.php
 if (session_status() === PHP_SESSION_NONE) {
     $lifetime = 600;
     session_set_cookie_params($lifetime, '/');
@@ -9,6 +8,8 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once('../Model/database.php');
 require_once('../Model/signup_func.php');
 
+require_once('../PHPMailer/PHPMailerAutoload.php');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username']);
     $email    = trim($_POST['email']);
@@ -16,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $confirm_password = $_POST['confirm_password'];
     $is_admin = isset($_POST['is_admin']) ? 1 : 0;
 
-    // Validation
+    
     if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
         $_SESSION['error_message'] = "All fields are required.";
         header("Location: ../index.php?action=signup");
@@ -26,15 +27,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: ../index.php?action=signup");
         exit;
     } else {
-        // Ask the model to do the heavy lifting
         $result = register_user($pdo, $username, $email, $password, $is_admin);
 
         if ($result === true) {
-            // SUCCESS: Set message and redirect clean out to your Login view!
             $_SESSION['success_message'] = "Thanks for signing up! Please log in below.";
-            
             $to_address = $email;
-            $to_name = $first_name . " " . $last_name;
+            $to_name = $username; 
             $from_address = 'fitworld6767@gmail.com';
             $from_name = 'FitWorld';
 
@@ -43,23 +41,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     '<p>Sincerely,</p>' .
                     '<p>FitReserve</p>';
 
-            $is_body_html = true;
-
+            
             try {
-                send_email($to_address, $to_name, $from_address, $from_name, 
-                        $subject, $body, $is_body_html);
-                include 'view/success.php';
-            } catch (Exception $ex) {
-                $error = $ex->getMessage();
-                include 'view/signup_page.php';
+                $mail = new PHPMailer(true); 
+
+                $mail->isSMTP();
+                $mail->Host       = 'smtp.gmail.com';
+                $mail->SMTPAuth   = true;
+                $mail->Username   = 'fitworld6767@gmail.com';
+                $mail->Password   = 'wuhm hpxh qafj mdmm'; 
+                $mail->SMTPSecure = 'tls'; 
+                $mail->Port       = 587;
+
+                $mail->setFrom($from_address, $from_name);
+                $mail->addAddress($to_address, $to_name);
+
+                $mail->isHTML(true);
+                $mail->Subject = $subject;
+                $mail->Body    = $body;
+
+                $mail->send();
+            } catch (Exception $e) {
+                error_log("PHPMailer failed: " . $mail->ErrorInfo);
             }
-
-            #break;
-
-            header("Location: ../index.php"); 
+            header("Location: ../index.php?action=login"); 
             exit;
         } else {
-            // FAILURE: Send back the database conflict string
             $_SESSION['error_message'] = $result;
             $_SESSION['form_input'] = [
                 'username' => $username,
